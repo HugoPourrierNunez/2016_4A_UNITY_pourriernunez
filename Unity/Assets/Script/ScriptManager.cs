f1eb43b2fc3bb8ac8b17d17963a6865b586507f5
Maxime Rignault <maximerignault@gmail.com>
2016-10-25 14:32:30 +0200

    Implemented collisions and created test scene

---
+++ Unity/Assets/Script/ScriptManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptManager : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject collision;
+
+    [SerializeField]
+    public GameObject[] bombs;
+
+	// Use this for initialization
+	void Start ()
+    {
+        foreach (GameObject bomb in bombs)
+        {
+            bomb.SetActive(true);
+        }
+        collision.SetActive(true);
+    }
+}
