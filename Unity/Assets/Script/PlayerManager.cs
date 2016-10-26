ecb5f3c2c1c0e4836595b9b6ce65affca9b0e479
Foucauld Bouvattier <Foucauld@Portable-Fouc.(none)>
2016-10-26 14:51:29 +0200

    Add player manager : selection d'objets

---
+++ Unity/Assets/Script/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SelectObject();
+	}
+
+    void SelectObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+            }
+        }
+    }
+
+
+}
