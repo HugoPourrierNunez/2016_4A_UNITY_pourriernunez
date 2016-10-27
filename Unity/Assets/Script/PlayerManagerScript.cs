6cca4430f253d841ff6a202580e2a0004891f649
anthonybazelle <a.bazelle1@laposte.net>
2016-10-27 17:30:16 +0200

    Ligne clic droit + Début du tool

--- Unity/Assets/Script/PlayerManagerScript.cs
+++ Unity/Assets/Script/PlayerManagerScript.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerManagerScript : MonoBehaviour {
+public class PlayerManagerScript : MonoBehaviour
+{
 
     GameManagerScript gameManagerScript;
+    private GameObject firstBombChoosen;
+    private LineRenderer lineX;
+    private LineRenderer lineY;
 
     public void setGameManagerScript(GameManagerScript gmScript)
     {
@@ -11,14 +15,19 @@ public class PlayerManagerScript : MonoBehaviour {
     }
 
     // Use this for initialization
-    void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        this.firstBombChoosen = null;
+        lineX = GetComponent<LineRenderer>();
+        lineY = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         TriggerExplosion();
-	}
+        TriggerLaser();
+    }
 
     void TriggerExplosion()
     {
@@ -28,11 +37,11 @@ public class PlayerManagerScript : MonoBehaviour {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 100.0f))//récupère la cible de la souris
             {
-                for(int i = 0; i < gameManagerScript.CollisionManagerScript.bombManagers.Length; i++)//parcours du tableau des bombes
+                for (int i = 0; i < gameManagerScript.CollisionManagerScript.bombManagers.Length; i++)//parcours du tableau des bombes
                 {
                     if (gameManagerScript.CollisionManagerScript.bombManagers[i].gameObject.Equals(hit.transform.gameObject))//identification de la cible dans le tableau
                     {
-                        if(gameManagerScript.ActualGameState.bombs[i].state != BombState.Explosion)//vérifie que la bombe n'est pas déjà en train d'exploser
+                        if (gameManagerScript.ActualGameState.bombs[i].state != BombState.Explosion)//vérifie que la bombe n'est pas déjà en train d'exploser
                         {
                             gameManagerScript.ActualGameState.bombs[i].state = BombState.Explosion;//mise à jour du bombstate
                             gameManagerScript.ActualGameState.bombs[i].delay = 2000;//initialisation du délai d'explosion
@@ -43,5 +52,28 @@ public class PlayerManagerScript : MonoBehaviour {
         }
     }
 
-
+    void TriggerLaser()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))//récupère la cible de la souris
+            {
+                for (int i = 0; i < gameManagerScript.CollisionManagerScript.bombManagers.Length; i++)//parcours du tableau des bombes
+                {
+                    if (gameManagerScript.CollisionManagerScript.bombManagers[i].gameObject.Equals(hit.transform.gameObject))//identification de la cible dans le tableau
+                    {
+                        lineX.SetPosition(0, new Vector3(gameManagerScript.CollisionManagerScript.bombManagers[i].gameObject.transform.position.x, 0, 20));
+                        lineX.SetPosition(1, new Vector3(gameManagerScript.CollisionManagerScript.bombManagers[i].gameObject.transform.position.x, 0, -20));
+                        lineX.SetWidth(0.2f, 0.2f);
+                        
+                        lineY.SetPosition(0, new Vector3(20, 0, gameManagerScript.CollisionManagerScript.bombManagers[i].gameObject.transform.position.z));
+                        lineY.SetPosition(1, new Vector3(-20, 0, gameManagerScript.CollisionManagerScript.bombManagers[i].gameObject.transform.position.z));
+                        lineX.SetWidth(0.2f, 0.2f);
+                    }
+                }
+            }
+        }
+    }
 }
